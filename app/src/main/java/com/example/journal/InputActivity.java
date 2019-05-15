0d71b21778fa9379d102fbe7e752f62785857175
levyvanderlinde <l-vdl@outlook.com>
2019-05-15 15:29:50 +0200

    UI's geupdate

--- app/src/main/java/com/example/journal/InputActivity.java
+++ app/src/main/java/com/example/journal/InputActivity.java
@@ -1,7 +1,9 @@
 package com.example.journal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class InputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
     }
+
+    // Onclick listener in case the user clicks on the floating button
+    public void Submit(View view) {
+
+        // Creating a new intent and navigating to next activity (Input Activity)
+        Intent i = new Intent(InputActivity.this, DetailActivity.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/journal/MainActivity.java
+++ app/src/main/java/com/example/journal/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.journal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Onclick listener in case the user clicks on the floating button
+    public void Createjournal(View view) {
+
+        // Creating a new intent and navigating to next activity (Input Activity)
+        Intent i = new Intent(MainActivity.this, InputActivity.class);
+        startActivity(i);
+    }
+
+
+
 }
+
